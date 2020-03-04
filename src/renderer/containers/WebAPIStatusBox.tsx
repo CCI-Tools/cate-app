bf4f631fdb0b2e67f7768d6ec03735220bb2b0b0
Norman Fomferra <norman.fomferra@gmail.com>
2020-03-04 13:27:52 +0100

    - Center status icon - Removed console warning saying that <div> cannot be in <p>

--- src/renderer/containers/WebAPIStatusBox.tsx
+++ src/renderer/containers/WebAPIStatusBox.tsx
@@ -22,32 +22,32 @@ function mapStateToProps(state: State): IWebAPIStatusBoxProps {
 const _WebAPIStatusBox: React.FC<IWebAPIStatusBoxProps & IDispatch> = (props) => {
     switch (props.webAPIStatus) {
         case 'login':
-            return (<AlertBox
+            return (<StatusBox
                 message={'Logging in...'}
                 icon="log-in"
                 isWaiting={true}
             />);
         case 'launching':
-            return (<AlertBox
+            return (<StatusBox
                 message={'Launching Cate service instance...'}
                 icon="log-in"
                 isWaiting={true}
             />);
         case 'connecting':
-            return (<AlertBox
+            return (<StatusBox
                 message={'Connecting to Cate service instance...'}
                 icon="log-in"
                 isWaiting={true}
             />);
         case 'logoff':
-            return (<AlertBox
+            return (<StatusBox
                 message={'Closing Cate service instance...'}
                 icon="log-out"
                 isWaiting={true}
             />);
         case 'closed':
         case 'error':
-            return (<AlertBox
+            return (<StatusBox
                 message={'Oops! The connection to the Cate service has been closed unexpectedly.'}
                 icon="offline"
                 isWaiting={false}
@@ -62,7 +62,7 @@ const _WebAPIStatusBox: React.FC<IWebAPIStatusBoxProps & IDispatch> = (props) =>
 const WebAPIStatusBox = connect(mapStateToProps)(_WebAPIStatusBox);
 export default WebAPIStatusBox;
 
-interface IAlertBoxProps {
+interface IStatusBoxProps {
     message: string;
     icon: IconName;
     isWaiting: boolean;
@@ -70,7 +70,10 @@ interface IAlertBoxProps {
     onCancel?: () => void;
 }
 
-const AlertBox: React.FC<IAlertBoxProps> = ({
+const DIALOG_P_STYLE: React.CSSProperties = {marginTop: 12};
+const DIALOG_ICON_STYLE: React.CSSProperties = {marginTop: 6, textAlign:'center', width: '100%'};
+
+const StatusBox: React.FC<IStatusBoxProps> = ({
                                                 message,
                                                 icon,
                                                 isWaiting,
@@ -80,7 +83,7 @@ const AlertBox: React.FC<IAlertBoxProps> = ({
 
     let progress;
     if (isWaiting) {
-        progress = (<p><ProgressBar intent={Intent.SUCCESS}/></p>);
+        progress = (<div style={DIALOG_P_STYLE}><ProgressBar intent={Intent.SUCCESS}/></div>);
     }
     let footer;
     if (onRetry || onCancel) {
@@ -102,8 +105,8 @@ const AlertBox: React.FC<IAlertBoxProps> = ({
             canOutsideClickClose={false}
         >
             <div className={Classes.DIALOG_BODY}>
-                <p><Icon icon={icon} iconSize={32}/></p>
-                <p>{message}</p>
+                <div  style={DIALOG_ICON_STYLE}><Icon icon={icon} iconSize={32}/></div>
+                <div style={DIALOG_P_STYLE}>{message}</div>
                 {progress}
             </div>
             {footer}
