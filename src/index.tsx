3617a2a2462ee0853737d39b1583ce1b9306846c
Norman Fomferra <norman.fomferra@gmail.com>
2020-11-30 17:59:09 +0100

    Use absolute CESIUM_BASE_URL

--- src/index.tsx
+++ src/index.tsx
@@ -18,8 +18,8 @@ declare global {
     }
 }
 
-window.CESIUM_BASE_URL = './cesium';
-// console.debug('CESIUM_BASE_URL =', window.CESIUM_BASE_URL);
+window.CESIUM_BASE_URL = new URL(`${window.location.href}/cesium`).href;
+console.debug('CESIUM_BASE_URL =', window.CESIUM_BASE_URL);
 
 main();
 
