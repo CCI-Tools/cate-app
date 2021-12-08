1c198b2b194556603d0dbeb3d6f60436da03a698
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2021-12-08 17:32:53 +0100

    Starting 3.1.1-dev.1

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -14,7 +14,7 @@
 // "./service-worker.js" file changes). Therefore we use a version number here,
 // so we can force updates.
 //
-const CATE_PWA_VERSION = "3.1.1-dev.0";
+const CATE_PWA_VERSION = "3.1.1-dev.1";
 
 console.debug(`Cate PWA version ${CATE_PWA_VERSION}`);
 
