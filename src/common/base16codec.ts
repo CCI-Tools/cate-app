219c1fe3aac6f6931c757c10f7a3773816798e89
Norman Fomferra <norman.fomferra@gmail.com>
2023-03-30 10:13:21 +0200

    reformat

--- src/common/base16codec.ts
+++ src/common/base16codec.ts
@@ -1,19 +1,19 @@
 class Base16 {
-  static encode(s: string): string {
-    let result = '';
-    for (let i = 0; i < s.length; i++) {
-      result += s.codePointAt(i).toString(16);
+    static encode(s: string): string {
+        let result = '';
+        for (let i = 0; i < s.length; i++) {
+            result += s.codePointAt(i).toString(16);
+        }
+        return result;
     }
-    return result;
-  }
 
-  static decode(s: string): string {
-    let result = '';
-    for (let i = 0; i < s.length; i += 2) {
-      result += String.fromCharCode(parseInt(s.substr(i, 2), 16));
+    static decode(s: string): string {
+        let result = '';
+        for (let i = 0; i < s.length; i += 2) {
+            result += String.fromCharCode(parseInt(s.substr(i, 2), 16));
+        }
+        return result;
     }
-    return result;
-  }
 }
 
 export default Base16;
